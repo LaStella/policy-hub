71b343c0b983762992500c13ecfe68d459e906c0
Bzeromo <dudrb5260@naver.com>
2023-04-25 18:31:59 +0900

    작업중

--- front/front-react/src/components/base/Header/index.jsx
+++ front/front-react/src/components/base/Header/index.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './header.css';
-import { Button } from '../../../components';
+import { Button, Modal, Spacer } from '../../../components';
 
-const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
-  <header>
-    <div className="wrapper" type='horizontal'>
-      <div class = "MainLogo"></div>
-      <div>
-        {user ? (
-          <>
-            <span className="welcome">
-              환영합니다, <b>{user.name}님</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+const Header = ({ user, onLogout }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <header>
+      <div className="wrapper" type='horizontal'>
+        <div class = "MainLogo"></div>
+        <div>
+          {user ? (
+            <>
+              <span className="welcome">
+                환영합니다, <b>{user.name}님</b>!
+              </span>
+              <Button size="small" onClick={onLogout} label="Log out" />
+            </>
+          ) : (
+            <>
+              <Spacer type="horizontal">
+                <span className="LoginPlease">
+                  맞춤 정보를 위해 로그인 하세요.
+                </span>
+                <Button size="small" onClick={() => setVisible(true)} label="Log in" />
+                <Modal.LoginModal visible={visible} onClose={() => setVisible(false)} onClick={() => setVisible(false)} />
+              </Spacer>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+}
 
 Header.propTypes = {
   user: PropTypes.shape({
--- front/front-react/src/components/base/Modal/LoginModal.js
+++ front/front-react/src/components/base/Modal/LoginModal.js
@@ -2,15 +2,33 @@ import Modal from "./index";
 import { Text, Button, Spacer } from "../../../components"
 import styled from "@emotion/styled";
 
-
 const LoginModal = ({ visible, onClose, onClick, ...props}) => {
     return (
         <div>
             <Modal.Base visible={visible} onClose={onClose}>
                 <div>
-                    <Spacer>
-                        <Text text-align="center" size={24} strong={true} block={true}>환영합니다</Text>
-                        <button onClick={onClick}>닫기</button>
+                    <Spacer type="vertical">
+                        <center>
+                            <Text size={40}>환영합니다</Text>
+                        </center>
+                        <Spacer size={15} type="vertical">
+                            <div>
+                                <Text text-align="left" size={24} block={true}> ◇ 부가 설명</Text>
+                            </div>
+                            <div>
+                                <Text text-align="left" size={24} block={true}> ◇ 부가 설명</Text>
+                            </div>
+                            <div>
+                                <Text text-align="left" size={24} block={true}> ◇ 부가 설명</Text>
+                            </div>
+                            <div>
+                                <Text text-align="left" size={24} block={true}> ◇ 부가 설명</Text>
+                            </div>
+                        </Spacer>
+                        <center>
+                            <Button primary={true} size={"large"} label={"자세히 확인하기"} onClick={onClick}></Button>
+                            <Button primary={false} size={"large"} label={"닫기"} onClick={onClick}></Button>
+                        </center>
                     </Spacer>
                 </div>
             </Modal.Base>
