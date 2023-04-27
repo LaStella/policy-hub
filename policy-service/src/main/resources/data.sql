8dfb5091e20fe73a93897fb25a9594b76f71b045
LaStella <jingyu.ham94@gmail.com>
2023-04-27 17:13:27 +0900

    chore: 정책 더미 데이터 추가

---
+++ policy-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into policies (policy_id, name, description, tag, logo, link)
+    values ('POLICY-001', '정책이름1', '정책설명1', '태그1', '로고1', '링크1');
+insert into policies (policy_id, name, description, tag, logo, link)
+    values ('POLICY-002', '정책이름2', '정책설명2', '태그2', '로고2', '링크2');
+insert into policies (policy_id, name, description, tag, logo, link)
+    values ('POLICY-003', '정책이름3', '정책설명3', '태그3', '로고3', '링크3');
